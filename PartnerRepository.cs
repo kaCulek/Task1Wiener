c0ece7df9f675f952304e48f677d047fd26051ee
kaCulek <karlo2316@gmail.com>
2025-01-04 21:25:13 +0100

    Connection string hardcode in PartnerRepository needed.

--- PartnerRepository.cs
+++ PartnerRepository.cs
@@ -1,56 +1,62 @@
 ﻿using Microsoft.Data.SqlClient;
 using Dapper;
-
+  
 namespace Task1Wiener
 {
     public class PartnerRepository
     {
         private readonly string _connectionString;
 
-        public PartnerRepository(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-
+     
         public PartnerRepository()
         {
-            _connectionString = "Server=.;Database=Task1Wiener;Trusted_Connection=True;";   
+            _connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
         }
 
         public async Task<IEnumerable<Partner>> GetAllPartnersAsync()
         {
             using var connection = new SqlConnection(_connectionString);
-            return await connection.QueryAsync<Partner>("SELECT * FROM Partners ORDER BY CreatedAtUtc DESC");
+            connection.Open();
+            var result1 = await connection.QueryAsync<Partner>("SELECT * FROM dbo.Partners ORDER BY CreatedAtUtc DESC");
+            return result1.ToList();
         }
 
         public async Task<Partner?> GetPartnerByIdAsync(int partnerId)
         {
             using var connection = new SqlConnection(_connectionString);
-            return await connection.QuerySingleOrDefaultAsync<Partner>("SELECT * FROM Partners WHERE PartnerId = @PartnerId", new { PartnerId = partnerId });
+            connection.Open();
+            var result2 = await connection.QuerySingleOrDefaultAsync<Partner>("SELECT * FROM dbo.Partners WHERE PartnerId = @PartnerId", new { PartnerId = partnerId });
+            return result2;
         }
 
         public async Task<int> AddPartnerAsync(Partner partner)
         {
             using var connection = new SqlConnection(_connectionString);
-            var sql = @"INSERT INTO Partners (FirstName, LastName, Address, PartnerNumber, CroatianPIN, PartnerTypeId, CreatedAtUtc, CreatedByUser, IsForeign, ExternalCode, Gender)
+            connection.Open();
+            var sql = @"INSERT INTO dbo.[Partners] (FirstName, LastName, Address, PartnerNumber, CroatianPIN, PartnerTypeId, CreatedAtUtc, CreatedByUser, IsForeign, ExternalCode, Gender)
                         VALUES (@FirstName, @LastName, @Address, @PartnerNumber, @CroatianPIN, @PartnerTypeId, @CreatedAtUtc, @CreatedByUser, @IsForeign, @ExternalCode, @Gender);
                         SELECT CAST(SCOPE_IDENTITY() as int)";
-            return await connection.ExecuteScalarAsync<int>(sql, partner);
+            var result3 = await connection.ExecuteScalarAsync<int>(sql, partner);
+            return result3;
         }
 
         public async Task<IEnumerable<Policy>> GetPoliciesByPartnerIdAsync(int partnerId)
         {
             using var connection = new SqlConnection(_connectionString);
-            return await connection.QueryAsync<Policy>("SELECT * FROM Policies WHERE PartnerId = @PartnerId", new { PartnerId = partnerId });
+            connection.Open();
+            var result4 = await connection.QueryAsync<Policy>("SELECT * FROM dbo.Policies WHERE PartnerId = @PartnerId", new { PartnerId = partnerId });
+            return result4.ToList();
         }
 
         public async Task<int> AddPolicyAsync(Policy policy)
         {
             using var connection = new SqlConnection(_connectionString);
-            var sql = @"INSERT INTO Policies (PartnerId, PolicyNumber, PolicyAmount)
+            connection.Open();
+            var sql = @"INSERT INTO dbo.Policies (PartnerId, PolicyNumber, PolicyAmount)
                         VALUES (@PartnerId, @PolicyNumber, @PolicyAmount);
                         SELECT CAST(SCOPE_IDENTITY() as int)";
-            return await connection.ExecuteScalarAsync<int>(sql, policy);
+            var result5 = await connection.ExecuteScalarAsync<int>(sql, policy);
+            return result5; 
         }
     }
 }
